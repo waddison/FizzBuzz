a0bd4582a789cc08dbd039aa16edd54c8bc6b873
Woody Addison <woody.addison@gmail.com>
2020-12-20 18:56:59 -0600

    Refactoring console application naming conventions for clarity

---
+++ ClassicApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SuperFizzBuzz;
+
+namespace ClassicApplication
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+
+            var results = fizzBuzz.CalculateRange();
+
+            foreach(var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
